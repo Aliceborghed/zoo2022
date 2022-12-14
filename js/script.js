01ffd1fe39de965df7974b6f79ce119cb8e89d17
Aliceborghed <120262617+Aliceborghed@users.noreply.github.com>
2022-12-14 10:25:36 +0100

    hamburgar meny
    har fixart med hamburarmenyn

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.navLinks')
+const burger = document.querySelector('.burger')
+
+function toggleMeny() {
+    nav.classList.toggle('navActive')
+    burger.classList.toggle('toggle')
+
+}
+
+
+burger.addEventListener('click', toggleMeny)
